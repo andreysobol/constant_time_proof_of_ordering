9ca530ea04813c9c85579e1dd23eec04df005451
Andrey Sobol <andreysobol@protonmail.com>
2021-10-13 05:16:41 +0300

    unittest

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+from order import OrderedElements
+
+class UnitTest(unittest.TestCase):
+
+    def test_hello_world(self):
+        assert(True)
+
+if __name__ == '__main__':
+    unittest.main()
